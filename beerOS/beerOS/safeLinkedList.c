1cd79993c75a9073ef879661d230b67ec6ea6894
Matthias Weis <matweis@htwg-konstanz.de>
2016-01-31 23:07:48 +0100

    init sema in safe ll, fixes #10, no tests available :(

--- beerOS/beerOS/safeLinkedList.c
+++ beerOS/beerOS/safeLinkedList.c
@@ -8,6 +8,7 @@ uint8_t safeLinkedList_init(SafeLinkedList *safeLinkedList, semaphore *sema){
 		return 1;
 	}
 	safeLinkedList->sema = sema;
+	initSemaphore(sema, 1);
 }
 
 uint8_t safeLinkedList_add(SafeLinkedList *safeLinkedList, void *item, uint8_t index){
